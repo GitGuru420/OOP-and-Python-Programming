e98c489259a1ed27d487a988480bbd77e6eafc4d
GitGuru420 <raisulislam1892@gmail.com>
2025-03-23 01:13:47 +0600

    Lecture Complete

---
+++ Week-01 Introduction to python/Module-01 Introduction to Python/Lecture/d_operators.py
@@ -0,0 +1,20 @@
+# arithmetic operators -> +, -, *, /, %, //
+num1 = 45
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3)
+
+# comparison operators -> >, <, >=, <=, ==, !==
+# no: ++, -- exist in python
+power = num1 ** num2
+print(power)
+
+# +=, -=, *=, /=
+a = 7
+# a = a + 5
+a += 5
---
+++ Week-01 Introduction to python/Module-01 Introduction to Python/Lecture/g_for_loop.py
@@ -0,0 +1,27 @@
+# for(int i=0; i<5; i++) {
+
+# }
+
+# numbers = [5, 10, 15, 20, 25]
+# sum = 0
+# for num in numbers:
+#     print(num)
+#     sum += num
+#     if sum > 20:
+#         print('bigger sum', sum)
+# print(sum)
+
+# text = 'Pagla Howar'
+# for char in text:
+#     print(char)
+
+# nums[1,2,3,4,5,6,7,8,9,10]
+# print(range(1,10))
+# for i in range(1,11):
+for i in range(1,11,2):
+    print(i)
+
+""" 
+Google Search: Kivave ekta python for loop e element er maan
+ebong element er index pabo ?
+"""
